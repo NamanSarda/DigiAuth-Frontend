f9f0375e713647d38d7e996e8875493f28c7da2a
nupurhada <hadanupur@gmail.com>
2024-09-27 00:28:30 +0530

    Credential card completed

--- app/formcheck/page.tsx
+++ app/formcheck/page.tsx
@@ -1,10 +1,9 @@
 "use client";
 import IssuanceForm from "../../components/Forms/IssuanceForm/form";
-
 export default function Home() {
   return (
-    <>
+    <div className="p-8">
       <IssuanceForm />
-    </>
+    </div>
   );
 }
--- components/Roles/User/Credentials/CredentialsCard.tsx
+++ components/Roles/User/Credentials/CredentialsCard.tsx
@@ -1,22 +1,57 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
+
+type Tattribute = {
+  key: string;
+  value: string;
+};
 
 type TcertificateProp = {
-  DID: string;
-  Name: string;
-  ID: string;
+  ConnectionID: string;
+  CredentialDefinition: string;
+  UserName: string;
+  IssuerName: string;
+  Attributes: Tattribute[];
   i: number;
 };
 
-export default function CredentialCard({ DID = "", Name, ID, i }: TcertificateProp) {
+export default function CredentialCard({
+  ConnectionID = "",
+  CredentialDefinition = "",
+  UserName = "",
+  IssuerName = "",
+  Attributes = [],
+}: TcertificateProp) {
   return (
-    <>
-      <Card className="bg-white p-4 shadow-sm">
-        <CardContent>
-          <h1>{`Connection Name :${Name}`}</h1>
-          <h2>{`Connection ID :${ID}`}</h2>
-          <p>{`DID:${DID}`}</p>
-        </CardContent>
-      </Card>
-    </>
+    <Card className=" p-4 shadow-lg border border-gray-200 rounded-xl">
+      <CardContent className="flex flex-col space-y-2">
+        <h1 className="text-md font-semibold ">
+          {`Connection ID: `}
+          <span className="font-normal">{ConnectionID}</span>
+        </h1>
+        <h2 className="text-md font-semibold ">
+          {`Credential Definition: `}
+          <span className="font-normal">{CredentialDefinition}</span>
+        </h2>
+        <p className="text-md font-semibold ">
+          {`User's Name: `}
+          <span className="font-normal">{UserName}</span>
+        </p>
+        <p className="text-md font-semibold ">
+          {`Issuer's Name: `}
+          <span className="font-normal">{IssuerName}</span>
+        </p>
+        <div>
+          <h3 className="text-md font-semibold ">Attributes:</h3>
+          <ul className="list-disc pl-5">
+            {Attributes.map((attr, index) => (
+              <li key={index} className="text-sm ">
+                <span className="font-bold ">{`${attr.key}: `}</span>
+                <span className="font-normal">{attr.value}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </CardContent>
+    </Card>
   );
 }
